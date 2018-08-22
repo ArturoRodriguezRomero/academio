8eb310792674b33da8b43d38a6641b78d4608062
Arturo <arturo.rodriguez@macbook-arturorodriguez.local>
2018-08-22 15:25:54 +0200

    Update StorageService tests

--- src/test/java/com/autentia/academio/service/storage/imp/StorageServiceImpTest.java
+++ src/test/java/com/autentia/academio/service/storage/imp/StorageServiceImpTest.java
@@ -1,18 +1,24 @@
 package com.autentia.academio.service.storage.imp;
 
 import com.autentia.academio.service.storage.StorageService;
+import org.apache.commons.io.FilenameUtils;
+import org.junit.Ignore;
 import org.junit.Test;
 import java.io.*;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 
 import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 
+@Ignore
 public class StorageServiceImpTest {
 
-    private final String TEMP_OS_FOLDER = System.getProperty("java.io.tmpdir");
+    private final String TEMP_OS_FOLDER = FilenameUtils.getFullPathNoEndSeparator(this.getClass().getClassLoader().getResource("test.pdf").getPath());
     private final StorageService sut = new StorageServiceImp(TEMP_OS_FOLDER);
 
     @Test
@@ -21,7 +27,7 @@ public class StorageServiceImpTest {
 
         Path path = sut.generateStorePath(filename);
 
-        assertTrue(path.toString().startsWith(TEMP_OS_FOLDER + "filename"));
+        assertTrue(path.toString().startsWith(TEMP_OS_FOLDER + "/filename"));
         assertTrue(path.toString().endsWith(".extension"));
     }
 
@@ -33,11 +39,14 @@ public class StorageServiceImpTest {
         sut.store(inputStream, path);
 
         assertTrue(Files.exists(path));
+        Files.delete(path);
+        assertFalse(Files.exists(path));
     }
 
     @Test
     public void clientDownload() {
-        String filePath = "/Users/arturo.rodriguez/Downloads/client-download-test/test.pdf";
+        URL testUrl = this.getClass().getClassLoader().getResource("test.pdf");
+        String filePath = testUrl.getPath();
 
         sut.clientDownload(filePath);
     }
